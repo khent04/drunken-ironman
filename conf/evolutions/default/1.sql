a2b303190c533cdcd63700c5d04a3809fdd35c86
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2014-03-16 10:21:49 -0400

    ...

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,9 +2,13 @@
 
 CREATE TABLE IF NOT EXISTS sticky_notes (
   id SERIAL PRIMARY KEY,
-  text varchar (256)
+  text varchar (256),
+  created_at TIMESTAMP
 );
 
+INSERT INTO sticky_notes (text,created_at) VALUES ('note A',NOW());
+INSERT INTO sticky_notes (text,created_at) VALUES ('note B',NOW());
+INSERT INTO sticky_notes (text,created_at) VALUES ('note C',NOW());
 
 # --- !Downs
 
